50e0b74e3dbe59a010e092fde9e139a5a04bcdc4
unknown <larserib@itk-d659.win.ntnu.no>
2016-09-11 16:31:27 +0200

    testcommit

---
+++ 3_3_8/UART_driver.c
@@ -0,0 +1,37 @@
+#include "UART_driver.h"
+
+FILE *mystdio;
+
+void UART_Init( unsigned int ubrr )
+{
+	// Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	
+	// Enable receiver and transmitter
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	
+	// Set frame: Register select - 2stop bit - 8data bit
+	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
+	
+	// Connect to printf
+	mystdio = fdevopen(&UART_Transmit,&UART_Receive);
+}
+
+unsigned char UART_Receive( void )
+{
+	/* Wait for data to be received */
+	while ( !(UCSR0A & (1<<RXC0)) )
+	;
+	/* Get and return received data from buffer */
+	return UDR0;
+}
+
+void UART_Transmit( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSR0A & (1<<UDRE0)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
